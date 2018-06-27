5858180ade595ecc755f34a2118ca280dc0dd7cb
Ralph <ralph@rgalan.es>
2018-06-27 16:16:02 +0100

    Double completed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-// Create a function which stores a number. Return an inner function which accepts a number as an argument, adds it to the number in parent scope and returns the new total
+// Create a function which stores a number. Return an inner 
+// function which accepts a number as an argument, adds it to 
+// the number in parent scope and returns the new total
 
-function increase(){
-
+function increase() {
   let number = 3;
-
-  function inner(newNumber){
+  function inner(newNumber) {
     number = newNumber + number;
     return number;
   }
+  return inner;
+}
+exports.increase = increase;
 
+
+// Create a function which stores a number. 
+// Return an inner function which doubles the number and returns it
+
+function double() {
+  let multiplier = 2;
+  function inner(number) {
+    return number * multiplier;
+  }
   return inner;
 }
 
-exports.increase = increase;
+exports.double = double;
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,11 +1,17 @@
-const { increase } = require('../src/index.js');
+const { increase, double, total, gibberish, mergeSort } = require('../src/index.js');
 
-test('increase', function(){
+test('increase', function () {
   const arg = 10;
   const inner = increase();
   const result = inner(arg);
-
-  const expected = 12;
-
+  const expected = 13;
   expect(result).toBe(expected);
 });
+
+
+test('double', function () {
+  const inner = double();
+  const result = inner(6);
+  const expected = 12;
+  expect(result).toBe(expected);
+})
